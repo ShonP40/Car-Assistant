480c12a91364aeb7aa47250304a9db59b351b0e9
ShonP40 <13995143+ShonP40@users.noreply.github.com>
2022-07-29 01:28:45 +0300

    Set ESP clock & Don't re-run these

--- src/modem.cpp
+++ src/modem.cpp
@@ -1,5 +1,9 @@
 #include "modem.h"
 
+bool initialized = false;
+
+bool timeSet = false;
+
 void light_sleep(uint32_t sec) {
   esp_sleep_enable_timer_wakeup(sec * 1000000ULL);
   esp_light_sleep_start();
@@ -15,44 +19,51 @@ TinyGsm modem(SerialAT);
 
 // Initialize the modem
 void initModem() {
-    Serial.println("\nInitializing modem...");
-    if (!modem.init()) {
-        Serial.println("Failed to restart modem, delaying 10s and retrying");
-        return;
+    if (!initialized) {
+        Serial.println("\nInitializing modem...");
+        if (!modem.init()) {
+            Serial.println("Failed to initiate the modem, retrying...");
+            initialized = false;
+            return;
+        } else {
+            initialized = true;
+        }
+
+        /*  Preferred mode selection : AT+CNMP
+            2  – Automatic
+            13 – GSM Only
+            14 – WCDMA Only
+            38 – LTE Only
+            59 – TDS-CDMA Only
+            9  – CDMA Only
+            10 – EVDO Only
+            19 – GSM+WCDMA Only
+            22 – CDMA+EVDO Only
+            48 – Any but LTE
+            60 – GSM+TDSCDMA Only
+            63 – GSM+WCDMA+TDSCDMA Only
+            67 – CDMA+EVDO+GSM+WCDMA+TDSCDMA Only
+            39 – GSM+WCDMA+LTE Only
+            51 – GSM+LTE Only
+            54 – WCDMA+LTE Only
+        */
+
+        modem.setNetworkMode(2);
+
+        // Unlock the SIM card with a PIN if needed
+        if (GSM_PIN && modem.getSimStatus() != 3) {
+            modem.simUnlock(GSM_PIN);
+        }
+
+        light_sleep(1);
     }
-
-    /*  Preferred mode selection : AT+CNMP
-        2  – Automatic
-        13 – GSM Only
-        14 – WCDMA Only
-        38 – LTE Only
-        59 – TDS-CDMA Only
-        9  – CDMA Only
-        10 – EVDO Only
-        19 – GSM+WCDMA Only
-        22 – CDMA+EVDO Only
-        48 – Any but LTE
-        60 – GSM+TDSCDMA Only
-        63 – GSM+WCDMA+TDSCDMA Only
-        67 – CDMA+EVDO+GSM+WCDMA+TDSCDMA Only
-        39 – GSM+WCDMA+LTE Only
-        51 – GSM+LTE Only
-        54 – WCDMA+LTE Only
-    */
-
-    modem.setNetworkMode(2);
-
-    // Unlock the SIM card with a PIN if needed
-    if (GSM_PIN && modem.getSimStatus() != 3) {
-        modem.simUnlock(GSM_PIN);
-    }
-
-    light_sleep(1);
 }
 
 // Get modem info
 void modemInfo() {
-    Serial.println((String)"\nModem Info: " + modem.getModemInfo());
+    if (!modem.isGprsConnected() && initialized) {
+        Serial.println((String)"\nModem Info: " + modem.getModemInfo());
+    }
 }
 
 String getPublicIP() {
@@ -75,62 +86,69 @@ String getPublicIP() {
 
 // Connect to the cellular network
 void initNetwork() {
-    bool res;
+    if (!modem.isGprsConnected() && initialized) {
+        bool res;
 
-    Serial.println("\nWaiting for network...");
-    if (!modem.waitForNetwork(600000L)) {
-        light_sleep(10);
-        return;
+        Serial.println("\nWaiting for network...");
+        if (!modem.waitForNetwork(600000L)) {
+            light_sleep(10);
+            return;
+        }
+
+        if (modem.isNetworkConnected()) {
+            Serial.println("Network connected");
+        }
+
+        Serial.println((String)"\nConnecting to: " + APN);
+        if (!modem.gprsConnect(APN, CELL_USER, CELL_PASS)) {
+            light_sleep(10);
+            return;
+        }
+
+        if (modem.isGprsConnected()) {
+            Serial.println("Connected to the cellular network");
+        } else {
+            Serial.println("Failed to connect to the cellular network");
+        }
+
+        Serial.println((String)"\nCCID: " + modem.getSimCCID());
+
+        Serial.println((String)"IMSI: " + modem.getIMSI());
+
+        Serial.println((String)"Operator: " + modem.getOperator());
+
+        Serial.println((String)"Signal quality: " + modem.getSignalQuality());
+
+        Serial.println((String)"\nPublic IP: " + getPublicIP() + "\n");
     }
-
-    if (modem.isNetworkConnected()) {
-        Serial.println("Network connected");
-    }
-
-    Serial.println((String)"\nConnecting to: " + APN);
-    if (!modem.gprsConnect(APN, CELL_USER, CELL_PASS)) {
-        light_sleep(10);
-        return;
-    }
-
-    if (modem.isGprsConnected()) {
-        Serial.println("Connected to the cellular network");
-    } else {
-        Serial.println("Failed to connect to the cellular network");
-    }
-
-    Serial.println((String)"\nCCID: " + modem.getSimCCID());
-
-    Serial.println((String)"IMSI: " + modem.getIMSI());
-
-    Serial.println((String)"Operator: " + modem.getOperator());
-
-    Serial.println((String)"Signal quality: " + modem.getSignalQuality());
-
-    Serial.println((String)"\nPublic IP: " + getPublicIP() + "\n");
 }
 
 // Get network time
 void getNetworkTime() {
-    int netyear = 0;
-    int netmonth = 0;
-    int netday = 0;
-    int nethour = 0;
-    int netmin = 0;
-    int netsec = 0;
-    float timezone = 0;
+    if (!timeSet && initialized) {
+        int netyear = 0;
+        int netmonth = 0;
+        int netday = 0;
+        int nethour = 0;
+        int netmin = 0;
+        int netsec = 0;
+        float timezone = 0;
 
-    for (int8_t i = 5; i; i--) {
-        Serial.println("Requesting current network time");
+        for (int8_t i = 5; i; i--) {
+            Serial.println("Requesting current network time");
 
-        if (modem.getNetworkTime(&netyear, &netmonth, &netday, &nethour, &netmin, &netsec, &timezone)) {
-            Serial.println((String)"Year: " + netyear + "\tMonth: " + netmonth + "\tDay: " + netday);
-            Serial.println((String)"Hour: " + nethour + "\tMinute: " + netmin + "\tSecond: " + netsec);
-            Serial.println((String)"Timezone: " + timezone);
-            break;
-        } else {
-            Serial.println("Couldn't get network time, retrying in 15s.");
-            light_sleep(15);
+            if (modem.getNetworkTime(&netyear, &netmonth, &netday, &nethour, &netmin, &netsec, &timezone)) {
+                Serial.println((String)"Year: " + netyear + "\tMonth: " + netmonth + "\tDay: " + netday);
+                Serial.println((String)"Hour: " + nethour + "\tMinute: " + netmin + "\tSecond: " + netsec);
+                Serial.println((String)"Timezone: " + timezone);
+                configTime(timezone, 0, "time.apple.com", "time.google.com");
+                break;
+            } else {
+                Serial.println("Couldn't get network time, retrying in 15s.");
+                light_sleep(15);
+            }
         }
+
+        timeSet = true;
     }
 }
