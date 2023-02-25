95066431c85395d3db5713a05516f773258dd571
ShonP40 <13995143+ShonP40@users.noreply.github.com>
2023-02-25 02:57:28 +0200

    Remove redundant checks

--- src/sensors.cpp
+++ src/sensors.cpp
@@ -31,97 +31,93 @@ Adafruit_Sensor *bme_pressure = bme.getPressureSensor();
 Adafruit_Sensor *bme_humidity = bme.getHumiditySensor();
 
 void initSensors() {
-  if (sensorsenable == "true") {
-    if (sensorsenablebme280 == "true") {
-      // Initialize the BME280 sensor
-      if (!bme.begin(i2cAddress("BME280", sensorbme280i2caddress))) {
-        #if DEBUG
-        SerialMon.println("Could not find a valid BME280 sensor, check wiring!");
-        #endif
-      }
+  if (sensorsenablebme280 == "true") {
+    // Initialize the BME280 sensor
+    if (!bme.begin(i2cAddress("BME280", sensorbme280i2caddress))) {
+      #if DEBUG
+      SerialMon.println("Could not find a valid BME280 sensor, check wiring!");
+      #endif
     }
+  }
 
-    if (sensorsenablepir == "true") {
-      // Initialize the PIR sensor
-      pinMode(stringToInt(sensorpirpin), INPUT);
-    }
+  if (sensorsenablepir == "true") {
+    // Initialize the PIR sensor
+    pinMode(stringToInt(sensorpirpin), INPUT);
   }
 }
 
 void readSensors() {
-  if (sensorsenable == "true") {
-    // BME 280
-    if (sensorsenablebme280 == "true") {
-      sensors_event_t temp_event, pressure_event, humidity_event;
-      bme_temp->getEvent(&temp_event);
-      bme_pressure->getEvent(&pressure_event);
-      bme_humidity->getEvent(&humidity_event);
+  // BME 280
+  if (sensorsenablebme280 == "true") {
+    sensors_event_t temp_event, pressure_event, humidity_event;
+    bme_temp->getEvent(&temp_event);
+    bme_pressure->getEvent(&pressure_event);
+    bme_humidity->getEvent(&humidity_event);
 
+    #if DEBUG
+    SerialMon.print("Temperature = ");
+    SerialMon.print(temp_event.temperature);
+    SerialMon.println(" *C");
+
+    SerialMon.print("Pressure = ");
+    SerialMon.print(pressure_event.pressure);
+    SerialMon.println(" hPa");
+
+    SerialMon.print("Humidity = ");
+    SerialMon.print(humidity_event.relative_humidity);
+    SerialMon.println(" %");
+    #endif
+
+    packageAndSendMQTT(String(temp_event.temperature), mqttsensorsbme280temperature);
+    packageAndSendMQTT(String(pressure_event.pressure), mqttsensorsbme280pressure);
+    packageAndSendMQTT(String(humidity_event.relative_humidity), mqttsensorsbme280humidity);
+  }
+
+  // TSL2561
+  Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(i2cAddress("TSL2561", sensortsl2561i2caddress), 12345);
+
+  if (sensorsenabletsl2561 == "true") {
+    // Initialize the TSL2561 sensor
+    if (!tsl.begin()) {
       #if DEBUG
-      SerialMon.print("Temperature = ");
-      SerialMon.print(temp_event.temperature);
-      SerialMon.println(" *C");
-
-      SerialMon.print("Pressure = ");
-      SerialMon.print(pressure_event.pressure);
-      SerialMon.println(" hPa");
-
-      SerialMon.print("Humidity = ");
-      SerialMon.print(humidity_event.relative_humidity);
-      SerialMon.println(" %");
+      SerialMon.println("Could not find a valid TSL2561 sensor, check wiring!");
       #endif
-
-      packageAndSendMQTT(String(temp_event.temperature), mqttsensorsbme280temperature);
-      packageAndSendMQTT(String(pressure_event.pressure), mqttsensorsbme280pressure);
-      packageAndSendMQTT(String(humidity_event.relative_humidity), mqttsensorsbme280humidity);
     }
-
-    // TSL2561
-    Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(i2cAddress("TSL2561", sensortsl2561i2caddress), 12345);
-
-    if (sensorsenabletsl2561 == "true") {
-      // Initialize the TSL2561 sensor
-      if (!tsl.begin()) {
-        #if DEBUG
-        SerialMon.println("Could not find a valid TSL2561 sensor, check wiring!");
-        #endif
-      }
+  
+    // Set the gain
+    if (sensortsl2561gain == "auto") {
+      tsl.enableAutoRange(true);
+    } else if (sensortsl2561gain == "1x") {
+      tsl.setGain(TSL2561_GAIN_1X);
+    } else if (sensortsl2561gain == "16x") {
+      tsl.setGain(TSL2561_GAIN_16X);
+    } else {
+      #if DEBUG
+      SerialMon.println("Invalid TSL2561_GAIN value, please use \"auto\", \"1x\" or \"16x\"");
+      #endif
+    }
     
-      // Set the gain
-      if (sensortsl2561gain == "auto") {
-        tsl.enableAutoRange(true);
-      } else if (sensortsl2561gain == "1x") {
-        tsl.setGain(TSL2561_GAIN_1X);
-      } else if (sensortsl2561gain == "16x") {
-        tsl.setGain(TSL2561_GAIN_16X);
-      } else {
-        #if DEBUG
-        SerialMon.println("Invalid TSL2561_GAIN value, please use \"auto\", \"1x\" or \"16x\"");
-        #endif
-      }
-      
-      sensors_event_t light_event;
-      tsl.getEvent(&light_event);
+    sensors_event_t light_event;
+    tsl.getEvent(&light_event);
 
-      #if DEBUG
-      SerialMon.print("Light = ");
-      SerialMon.print(light_event.light);
-      SerialMon.println(" lux");
-      #endif
+    #if DEBUG
+    SerialMon.print("Light = ");
+    SerialMon.print(light_event.light);
+    SerialMon.println(" lux");
+    #endif
 
-      packageAndSendMQTT(String(light_event.light), mqttsensorstsl2561lux);
-    }
+    packageAndSendMQTT(String(light_event.light), mqttsensorstsl2561lux);
+  }
 
-    // PIR
-    if (sensorsenablepir == "true") {
-      int pirState = digitalRead(stringToInt(sensorpirpin));
+  // PIR
+  if (sensorsenablepir == "true") {
+    int pirState = digitalRead(stringToInt(sensorpirpin));
 
-      #if DEBUG
-      SerialMon.print("PIR = ");
-      SerialMon.println(pirState);
-      #endif
+    #if DEBUG
+    SerialMon.print("PIR = ");
+    SerialMon.println(pirState);
+    #endif
 
-      packageAndSendMQTT(String(pirState), mqttsensorspir);
-    }
+    packageAndSendMQTT(String(pirState), mqttsensorspir);
   }
 }
